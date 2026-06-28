62c371db164ffc16fbce3d794639ec3cbf74b927
obaid <obaidullahshaikh07@gmail.com>
2026-06-28 18:38:03 +0530

    feat: add GFTLogo component and implement premium green and gold branding in global styles

--- components/GFTLogo.js
+++ components/GFTLogo.js
@@ -4,11 +4,12 @@ import React from "react";
 
 export default function GFTLogo({ className = "h-8 w-auto", showText = true, light = false }) {
   return (
-    <div className="flex items-center select-none group/logo">
+    <div className="flex items-center select-none group/logo pointer-events-none">
       <img
         src="/logo.png"
-        alt="Green Future Technology"
-        className={`${className} object-contain transition-transform duration-300 group-hover/logo:scale-105`}
+        alt=""
+        className={`${className} object-contain transition-transform duration-300 group-hover/logo:scale-105 select-none pointer-events-none`}
+        draggable="false"
       />
     </div>
   );
